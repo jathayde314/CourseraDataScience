ba2deb92ca8d0e033c4cafb54a3d81c857041a23
Joshua Athayde <joshua.athayde@gmail.com>
2020-09-05 21:22:51 -0700

    Calculated average cases per county

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import csv
 
+
+data = []
 with open("/Users/joshuaathayde/github/CourseraDataScience/statewide_cases.csv", mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
-    print(csv_reader)
-    for row in csv_reader: pass
+    for row in csv_reader: data.append(row)
 
-print(row)
+#Average coronavirus cases in county:
+sum = 0
+totalCounties = 0
+for county in data[1:]:
+    if county['totalcountconfirmed'] != '':
+        sum += float(county['totalcountconfirmed'])
+        totalCounties += 1
+print(sum/totalCounties)
