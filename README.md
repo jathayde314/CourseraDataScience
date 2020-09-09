64bd233a38f59fdc1b4ab1b7c9cc4bc4b5fd97a7
Joshua Athayde <joshua.athayde@gmail.com>
2020-09-08 20:48:24 -0700

    Played around with it some more

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CourseraDataScience
+
+### End Goal
+
+The end goal of this project is to analyze and visualize the California coronavirus data. Using regression, I would like to see if I can project the age, gender, and ethnic makeup of coronavirus cases in each county.
--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import csv
 
 
 data = []
-with open("/Users/joshuaathayde/github/CourseraDataScience/statewide_cases.csv", mode='r') as csv_file:
+with open("/Users/joshuaathayde/github/CourseraDataScience/case_demographics_age.csv", mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     for row in csv_reader: data.append(row)
-
+print(data[0].keys())
 #Average coronavirus cases in county:
 sum = 0
 totalCounties = 0
 for county in data[1:]:
-    if county['totalcountconfirmed'] != '':
-        sum += float(county['totalcountconfirmed'])
+    if county['age_group'] != '':
+        sum += float(county['totalpositive'])
         totalCounties += 1
 print(sum/totalCounties)
