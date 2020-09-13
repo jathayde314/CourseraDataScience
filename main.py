a7b6a00cdc8963eaaafc1c4098cfa64b5ef70fb4
Joshua Athayde <joshua.athayde@gmail.com>
2020-09-12 22:49:51 -0700

    Found keys for all csv files.

--- main.py
+++ main.py
@@ -11,16 +11,19 @@ statewide = []
 with open("/Users/joshuaathayde/github/CourseraDataScience/statewide_cases.csv", mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     for row in csv_reader: statewide.append(row)
+print(statewide[0].keys())
 
 gender = []
 with open("/Users/joshuaathayde/github/CourseraDataScience/case_demographics_sex.csv", mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     for row in csv_reader: gender.append(row)
+print(gender[0].keys())
 
 ethnicity = []
 with open("/Users/joshuaathayde/github/CourseraDataScience/case_demographics_ethnicity.csv", mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     for row in csv_reader: ethnicity.append(row)
+print(ethnicity[0].keys())
 
 #Average coronavirus cases in county:
 sum = 0
